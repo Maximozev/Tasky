b2bb05e1e57328b906a51453db1ddc8b62d04e04
Maximozev <themaximozevyt@gmail.com>
2024-12-06 15:54:21 -0500

    added new link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,11 @@ const Navbar = () => {
     <div className="flex py-3 flex-wrap justify-around">
       <h1 className="text-lg font-semibold">Tasky</h1>
       <ul className="flex gap-[40px] text-m">
-        <li>Home</li>
+        <li>
+          <a href="#" className="text-gray-600 hover:text-gray-900">
+            Home
+          </a>
+        </li>
         <li>
           {" "}
           <a href="https://github.com/Maximozev?tab=repositories">Products</a>
